6528b5677240095cf68ab0708020656447622a51
archit_chandra <send2archit.chandra@gmail.com>
2020-01-18 18:42:32 +0100

    added variables & method returning String/Number

--- my-first-project/src/app/server/server.component.ts
+++ my-first-project/src/app/server/server.component.ts
@@ -1,9 +1,14 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector:'app-server',
-  templateUrl:'./server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offLine';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
