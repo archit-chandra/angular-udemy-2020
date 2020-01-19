b21e71854d5d810ce65d1796c4d1ea615404f39c
archit_chandra <send2archit.chandra@gmail.com>
2020-01-19 03:42:33 +0100

    added a new username component

--- my-first-project-assignments/src/app/app.module.ts
+++ my-first-project-assignments/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule
