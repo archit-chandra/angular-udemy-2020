666be11def9f4c4e7546530e62576e7124c353e3
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-20 22:24:50 +0100

    Binding to custom property: temp commented to remove compilation error.

--- component-databinding-deep/src/app/cockpit/cockpit.component.ts
+++ component-databinding-deep/src/app/cockpit/cockpit.component.ts
@@ -15,19 +15,21 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    this.serverElements.push({
+    // Temporary: To remove compilation error. It will be implemented later.
+    /*this.serverElements.push({
       type: 'server',
       name: this.newServerName,
       content: this.newServerContent
-    });
+    });*/
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
+    // Temporary: To remove compilation error. It will be implemented later.
+    /*this.serverElements.push({
       type: 'blueprint',
       name: this.newServerName,
       content: this.newServerContent
-    });
+    });*/
   }
 
 }
