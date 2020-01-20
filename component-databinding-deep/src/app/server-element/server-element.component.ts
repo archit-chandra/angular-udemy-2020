968d15503cf92273e4d338fdaae9ef74825e249a
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-20 23:19:05 +0100

    view encapsulation

--- component-databinding-deep/src/app/server-element/server-element.component.ts
+++ component-databinding-deep/src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
+  // To remove Shadow DOM id appended by Angular to its elements
+  /*encapsulation: ViewEncapsulation.None // Emulated(default), Native*/
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: { type: string, name: string, content: string };
