2fad4b61283d823a747c1b1a9e8222a9b50fb6b3
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-22 14:17:17 +0100

    Used HostBinding to bind to host properties - alternative to use renderer

--- directives-deep/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives-deep/src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,22 +1,26 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    this.backgroundColor = 'green';
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    this.backgroundColor = 'green';
   }
 }
