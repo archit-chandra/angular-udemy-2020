f434997aa72e88c11eff6d59399f23b37589bbb5
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-22 15:08:52 +0100

    Building custom structural directive: used custom structural directive

--- directives-deep/src/app/app.module.ts
+++ directives-deep/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
