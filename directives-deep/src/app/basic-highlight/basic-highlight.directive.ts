74d6a4db46d1e10516567fb65081d4eb708f3b99
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-22 14:42:39 +0100

    Binding to directive property: to let the user override the default values provided by the directive

--- directives-deep/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives-deep/src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,9 +1,11 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'yellow';
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
@@ -11,16 +13,16 @@ export class BasicHighlightDirective implements OnInit {
 
   ngOnInit(): void {
     //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.defaultColor;
   }
 }
