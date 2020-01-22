25ace55040b2508682a212e922edaafa96cc1abb
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-22 13:59:26 +0100

    Basic Directive: created basic directive

---
+++ directives-deep/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,14 @@
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighlight]'
+})
+export class BasicHighlightDirective implements OnInit {
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+  }
+}
