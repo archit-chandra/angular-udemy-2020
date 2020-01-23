dbbe53b30c696afdd3b7f56e9a835019f28537cc
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-23 22:20:12 +0100

    routing: bug fixing- router path  to '' + right UsersComponent

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -14,8 +14,8 @@ import {ServersService} from './servers/servers.service';
 import {RouterModule, Routes} from "@angular/router";
 
 const appRoutes: Routes = [
-  {path: '/', component: HomeComponent},
-  {path: 'users', component: UserComponent},
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent},
   {path: 'servers', component: ServersComponent}
 ];
 
