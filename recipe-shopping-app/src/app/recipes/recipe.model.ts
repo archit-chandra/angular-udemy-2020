ae31691b97125e6cc2adb7e6a4ef69777535ea48
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-23 15:13:11 +0100

    course directives: added ingredients to recipes

--- recipe-shopping-app/src/app/recipes/recipe.model.ts
+++ recipe-shopping-app/src/app/recipes/recipe.model.ts
@@ -1,4 +1,16 @@
+import {IngredientModel} from "../shared/ingredient.model";
+
 export class RecipeModel {
-  constructor(public name: string, public desc: string, public imagePath: string) {
+
+  public name: string;
+  public desc: string;
+  public imagePath: string;
+  public ingredients: IngredientModel[];
+
+  constructor(name: string, desc: string, imagePath: string, ingredients: IngredientModel[]) {
+    this.name = name;
+    this.desc = desc;
+    this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- recipe-shopping-app/src/app/recipes/recipe.service.ts
+++ recipe-shopping-app/src/app/recipes/recipe.service.ts
@@ -1,18 +1,28 @@
 import {RecipeModel} from './recipe.model';
 import {EventEmitter} from "@angular/core";
+import {IngredientModel} from "../shared/ingredient.model";
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<RecipeModel>();
 
   private recipies: RecipeModel[] = [
-    new RecipeModel('TestRecipe',
-      'This is test recipe',
-      'https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg'),
-    new RecipeModel('TestRecipe2',
-      'This is another test recipe',
-      'https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg')
-  ];
+    new RecipeModel(
+      'Schnitzel',
+      'This is delicious',
+      'https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg',
+      [
+        new IngredientModel('Meat', 1),
+        new IngredientModel('French Fries', 20)
+      ]),
+    new RecipeModel(
+      'Burger',
+      'This is heavy meal',
+      'https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg',
+      [
+        new IngredientModel('Buns', 2),
+        new IngredientModel('Meat', 1)
+      ])];
 
   getRecipes() {
     return this.recipies.slice(); // slice will return the copy
