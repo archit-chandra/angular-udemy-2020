4f47afc9a877acee679d27d5182910259c375de1
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-23 14:54:28 +0100

    course directives: used shopping-list service (add ingredient, get ingredients)

--- recipe-shopping-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-shopping-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,23 @@
 import {Component, OnInit} from "@angular/core";
 import {IngredientModel} from '../shared/ingredient.model';
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: IngredientModel[] = [
-    new IngredientModel('Apple', 5),
-    new IngredientModel('Tomatoes', 10),
-  ];
+  ingredients: IngredientModel[] = [];
 
-  constructor() {
+  constructor(private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit() {
-  }
-
-  onIngredientAdded(ingredient: IngredientModel) {
-    this.ingredients.push(ingredient);
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: IngredientModel[]) => {
+        this.ingredients = ingredients;
+      });
   }
 }
