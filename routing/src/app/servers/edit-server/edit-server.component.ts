1619aec312c080369413af6141a38f0f9031284c
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-24 17:11:42 +0100

    routing: retrieving query parameters and fragments

--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { ServersService } from '../servers.service';
+import {ServersService} from '../servers.service';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-edit-server',
@@ -8,13 +9,24 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    // this approach won't work because it will not update itself if the component is already loaded.
+    // it is not reactive. it will only work for the first time the component is loaded though.
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+
+    // this makes reactive
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
