fbfc9651c2f716077c10436c185f258eca6eeb18
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-25 02:06:59 +0100

    routing: passing static data to route (via new ErrorPageComponent)

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import {UserComponent} from "./users/user/user.component";
 import {ServersComponent} from "./servers/servers.component";
 import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {NgModule} from "@angular/core";
 import {AuthGuard} from "./auth-guard.service";
 import {CanDeactivateGuard} from "./can-deactivate-guard.service";
+import {ErrorPageComponent} from "./error-page/error-page.component";
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -24,7 +24,8 @@ const appRoutes: Routes = [
       {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
     ]
   },
-  {path: 'not-found', component: PageNotFoundComponent},
+  // {path: 'not-found', component: PageNotFoundComponent},
+  {path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
 
   // NOTES: '**' must be the last one in array of routes
   {path: '**', redirectTo: '/not-found'}
