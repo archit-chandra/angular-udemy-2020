f04186d78ef067b4714e4a4c171cc9cdb5c46578
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-25 01:35:14 +0100

    routing: bugfixing - editing server dynamically (it was hardcoded, so it was only updating the same server each time)

--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -32,7 +32,13 @@ export class EditServerComponent implements OnInit {
     );
     this.route.fragment.subscribe();
 
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
