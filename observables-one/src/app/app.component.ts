810f79246de970fae5e7e163af59ba85a775025b
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-27 03:55:48 +0100

    Observables: subject - unsubscribe (always)

--- observables-one/src/app/app.component.ts
+++ observables-one/src/app/app.component.ts
@@ -1,20 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from "./user/user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  private userActivatedSubscription: Subscription;
 
   constructor(private userService: UserService) {
   }
 
   ngOnInit() {
-    this.userService.activatedUserEmitter.subscribe(isUserActivated => {
+    this.userActivatedSubscription = this.userService.activatedUserEmitter.subscribe(isUserActivated => {
       this.userActivated = isUserActivated;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.userActivatedSubscription.unsubscribe();
   }
 }
