2137916a6a8d0a2e26645d2427110fcd7800e617
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-27 02:38:45 +0100

    Observables: unsubscribe from observables when leaving the component (on destroy)

--- observables-one/src/app/home/home.component.ts
+++ observables-one/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private fisrtObsSubscription: Subscription;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.fisrtObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.fisrtObsSubscription.unsubscribe();
   }
 
 }
