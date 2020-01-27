a0bc90b8310cce529901de0e403e69620f33b436
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-27 03:00:48 +0100

    Observables: building a custom observable (with error)

--- observables-one/src/app/home/home.component.ts
+++ observables-one/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {interval, Subscription} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -14,8 +14,25 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.fisrtObsSubscription = interval(1000).subscribe(count => {
+    /*this.fisrtObsSubscription = interval(1000).subscribe(count => {
       console.log(count);
+    })*/
+
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
+        count++;
+      }, 1000)
+    });
+
+    this.fisrtObsSubscription = customIntervalObservable.subscribe(count => {
+      console.log(count);
+    }, error => {
+      alert(error.message);
     })
   }
 
