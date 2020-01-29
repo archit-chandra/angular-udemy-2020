632f5bc673982dab833e528784992330156f1863
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-29 18:06:12 +0100

    HTTP-requests: using RxJS operators to transform response data

--- http-requests/src/app/app.component.ts
+++ http-requests/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -31,11 +32,21 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://angular-2020-6c98c.firebaseio.com/posts.json').subscribe(
-      (posts) => {
-        console.log(posts);
-      }
-    );
+    this.http.get('https://angular-2020-6c98c.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(
+        (posts) => {
+          console.log(posts);
+        }
+      );
   }
 
   onClearPosts() {
