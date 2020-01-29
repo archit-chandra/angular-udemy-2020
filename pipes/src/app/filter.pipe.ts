35d27db6c1c7c9bd2ac4ac45128751c839688daa
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-29 15:16:41 +0100

    Pipes: pure and impure pipes (re-run pipes when pipe input data changes)

--- pipes/src/app/filter.pipe.ts
+++ pipes/src/app/filter.pipe.ts
@@ -1,7 +1,9 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  // NOTES: re-run pipe when input data changes (cost performance)
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
