22e330d3696749c46dee4f9a2f33203e68d15080
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-30 01:01:12 +0100

    course-HTTP-requests: setting up the database storage service

---
+++ recipe-shopping-app/src/app/shared/data-storage.service.ts
@@ -0,0 +1,9 @@
+import {Injectable} from "@angular/core";
+import {HttpClientModule} from "@angular/common/http";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+
+  constructor(private http: HttpClientModule) {
+  }
+}
