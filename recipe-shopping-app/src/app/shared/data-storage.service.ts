c3511b145d74b7b6d81ffe05d4aa0386b1c83a98
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-30 13:47:53 +0100

    course-HTTP-requests: bug fixing -  resolving data before loading

--- recipe-shopping-app/src/app/shared/data-storage.service.ts
+++ recipe-shopping-app/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {RecipeModel} from "../recipes/recipe.model";
-import {map} from "rxjs/operators";
+import {map, tap} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -21,15 +21,13 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<RecipeModel[]>('https://angular8course-recipe-shopping.firebaseio.com/recipes.json')
+    return this.http.get<RecipeModel[]>('https://angular8course-recipe-shopping.firebaseio.com/recipes.json')
       .pipe(map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
         });
-      }))
-      .subscribe(recipes => {
-        console.log(recipes);
+      }), tap(recipes => {
         this.recipeService.setRecipes(recipes);
-      });
+      }));
   }
 }
