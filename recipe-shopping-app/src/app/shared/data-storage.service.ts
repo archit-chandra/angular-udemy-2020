381734771fc9a5ec68c6a0d04dc0eb5ec2fbe6d5
Archit Chandra <send2archit.chandra@gmail.com>
2020-01-30 01:41:40 +0100

    course-HTTP-requests: storing recipes

--- recipe-shopping-app/src/app/shared/data-storage.service.ts
+++ recipe-shopping-app/src/app/shared/data-storage.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from "@angular/core";
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
+import {RecipeService} from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
 
-  constructor(private http: HttpClientModule) {
+  constructor(private http: HttpClient,
+              private  recipeService: RecipeService) {
+  }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    // using put (FIREBASE-specific) to override existing recipe in FIREBASE DB
+    this.http.put('https://angular8course-recipe-shopping.firebaseio.com/recipes.json', recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 }
